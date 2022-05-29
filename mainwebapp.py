09485db51f9e8dd5591e50f78d9a9b410eba799a
Pushpa Yadav <75825058+ypushpa9420@users.noreply.github.com>
2022-05-29 19:15:43 +0530

    Update mainwebapp.py

--- mainwebapp.py
+++ mainwebapp.py
@@ -11,7 +11,7 @@ from apps1 import singleprediction,multipleprediction,edavda,intro
 
 app = MultiApp()
 
-st.sidebar.header("CREDIT RISK ASSESSMENT TOOL")
+st.sidebar.header("**CREDIT RISK ASSESSMENT TOOL**")
 
 # st.markdown('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">', unsafe_allow_html=True)
 
