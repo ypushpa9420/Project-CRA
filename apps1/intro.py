0e83dc6326a7ebc5d806191fc2e676e030e2a542
Pushpa Yadav <75825058+ypushpa9420@users.noreply.github.com>
2022-05-29 19:15:20 +0530

    Update intro.py

--- apps1/intro.py
+++ apps1/intro.py
@@ -11,9 +11,9 @@ import streamlit as st
 
 def app():
     
-   st.write("Introduction")
+   st.header("**Introduction**")
    st.write("Credit default risk is simply known as the possibility of a loss for a lender due to a borrower’s failure to repay a loan. Credit analysts are typically responsible for assessing this risk by thoroughly analyzing a borrower’s capability to repay a loan — but long gone are the days of credit analysts, it’s the machine learning age! Machine learning algorithms have a lot to offer to the world of credit risk assessment due to their unparalleled predictive power and speed. In this article, we will be utilizing machine learning’s power to predict whether a borrower will default on a loan or not and to predict their probability of default. Let’s get started.")
-   st.write("Dataset")
+   st.header("**Dataset**")
    st.write("The dataset we’re using can be found on Kaggle and it contains data for 32,581 borrowers and 11 variables related to each borrower. Let’s have a look at what those variables are:")
 
    st.write("Age - numerical variable; age in years")
